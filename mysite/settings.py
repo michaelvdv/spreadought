f8deff16e44459d39b4a973edba42d1f97efbea1
Michael <m.f.v.d.veen@student.tue.nl>
2017-01-08 00:42:34 +0100

    change host to pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'poll',
         'USER': 'django',
         'PASSWORD': 'django',
-        'HOST':'.herokuapp',
+        'HOST':'.pythonanywhere.com',
         'PORT':'',
     }
 }
